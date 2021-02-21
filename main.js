79bb5365dd66f13847086ca302b0aed25582b736
Shachin <shachinchakma85@gmail.com>
2021-02-22 02:55:13 +0600

    Finally Done The Position

---
+++ main.js
@@ -0,0 +1,7 @@
+function clicked() {
+    document.getElementById("one-bar").classList.toggle('upper-bar-clicked');
+
+    document.getElementById("two-bar").classList.toggle('lower-bar-clicked');
+
+    document.getElementById("three-bar").classList.toggle('text-clicked');
+}
