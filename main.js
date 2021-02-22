517d4149447bf77407dc45bdd61e850100d52254
pranta-barua007 <prantabarua247@gmail.com>
2021-02-22 18:38:42 +0600

    logic

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+var clickd = false;
 function clicked() {
+    if (clickd) {
+        document.getElementById("one-bar").classList.toggle('after-cross-upper-bar');
+
+        clickd = false
+
+        return
+    }
     document.getElementById("one-bar").classList.toggle('upper-bar-clicked');
 
     document.getElementById("two-bar").classList.toggle('lower-bar-clicked');
 
     document.getElementById("three-bar").classList.toggle('text-clicked');
+
+    clickd = true;
+    
 }
